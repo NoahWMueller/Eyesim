01fb6a01a04845ab7d05758258f6d5916460b04e
Terry Mueller <terry.mueller@campfire.com.au>
2025-05-19 19:18:40 +0800

    version 1.3

--- road_follow_PPO.py
+++ road_follow_PPO.py
@@ -23,7 +23,7 @@ DESIRED_CAMHEIGHT = 60
 algorithm = "PPO" 
 
 # Current version of the code for saving models and logs
-version = 1.2
+version = 1.3
 
 # Directory paths for saving models and logs
 models_dir = f"models/Carolo/{version}"
