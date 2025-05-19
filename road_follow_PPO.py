b9a744765677ad76404d53dc027cfd3d28ed7bb4
NoahWMueller <noah@mueller.com.au>
2025-05-19 17:34:42 +0800

    updating training script

--- road_follow_PPO.py
+++ road_follow_PPO.py
@@ -11,7 +11,6 @@ import gymnasium as gym
 from random import randint
 from gymnasium import spaces
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_checker import check_env
 
 # GLOBAL VARIABLES ---------------------------------------------------------------------------------------------------
 
@@ -24,7 +23,7 @@ DESIRED_CAMHEIGHT = 60
 algorithm = "PPO" 
 
 # Current version of the code for saving models and logs
-version = 1.2
+version = 1.3
 
 # Directory paths for saving models and logs
 models_dir = f"models/Carolo/{version}"
@@ -36,42 +35,52 @@ current_centroid = 0
 next_polygon = np.array([])
 next_centroid = 0
 
-# define the world coordinates for the left lane
-flipped_left_lane_world_coordinates = [
-    (3990,400),(4000,733),(943,410),(962,733),(590,543),
-    (771,790),(352,714),(600,943),(190,1010),(505,1057),
-    (114,1229),(438,1248),(114,3381),(429,3381),(181,3705),
-    (467,3590),(305,3981),(571,3829),(552,4257),(771,4019),
-    (819,4438),(981,4171),(1171,4562),(1190,4248),(4029,4581),
-    (4019,4267),(4524,4362),(4333,4133),(4800,4000),(4514,3895),
-    (4876,3619),(4562,3610),(4790,3248),(4524,3390),(4581,2943),
-    (4362,3162),(4333,2705),(4105,2905),(4124,2552),(3933,2733),
-    (3895,2314),(3667,2524),(3676,2086),(3438,2305),(3438,1867),
-    (3219,2076),(3257,1686),(3057,1905),(2981,1390),(2743,1610),
-    (2476,1229),(2476,1514),(1943,1352),(2114,1619),(1590,1743),
-    (1876,1867),(1486,2067),(1810,2105),(1476,2867),(1800,2857),
-    (1581,3171),(1838,3029),(1743,3448),(2000,3238),(2124,3695),
-    (2267,3400),(2590,3771),(2600,3448),(3095,3581),(2895,3343),
-    (3371,3314),(3143,3086),(3705,2981),(3457,2762),(4124,2524),
-    (3905,2305),(4390,2257),(4152,2048),(4695,1933),(4448,1724),
-    (4876,1457),(4562,1400),(4762,924),(4486,1086),(4486,600),
-    (4267,819),(3990,400),(4000,733)
+# define the world centroids for the left lane
+left_lane = [
+    (3990,400),(4000,733),(3562,400),(3571,733),(3171,400),
+    (3171,733),(2838,400),(2848,733),(2448,400),(2448,733),
+    (2095,410),(2095,733),(1676,400),(1657,743),(1267,410),
+    (1238,733),(943,410),(962,733),(590,543),(771,790),
+    (352,714),(600,943),(190,1010),(505,1057),(114,1229),
+    (438,1248),(114,1619),(438,1619),(114,1971),(438,2000),
+    (114,2362),(438,2371),(114,2705),(438,2733),(114,3086),
+    (438,3086),(114,3381),(429,3381),(181,3705),(467,3590),
+    (305,3981),(571,3829),(552,4257),(771,4019),(819,4438),
+    (981,4171),(1171,4562),(1190,4248),(1619,4562),(1619,4248),
+    (2000,4571),(2010,4248),(2410,4581),(2410,4267),(2762,4590),
+    (2762,4267),(3190,4581),(3190,4257),(3619,4581),(3619,4257),
+    (4029,4581),(4019,4267),(4524,4362),(4333,4133),(4800,4000),
+    (4514,3895),(4876,3619),(4562,3610),(4790,3248),(4524,3390),
+    (4581,2943),(4362,3162),(4333,2705),(4105,2905),(4124,2552),
+    (3933,2733),(3895,2314),(3667,2524),(3676,2086),(3438,2305),
+    (3438,1867),(3219,2076),(3257,1686),(3057,1905),(2981,1390),
+    (2743,1610),(2476,1229),(2476,1514),(1943,1352),(2114,1619),
+    (1590,1743),(1876,1867),(1486,2067),(1810,2105),(1476,2476),
+    (1800,2476),(1476,2867),(1800,2857),(1581,3171),(1838,3029),
+    (1743,3448),(2000,3238),(2124,3695),(2267,3400),(2590,3771),
+    (2600,3448),(3095,3581),(2895,3343),(3371,3314),(3143,3086),
+    (3705,2981),(3457,2762),(4124,2524),(3905,2305),(4390,2257),
+    (4152,2048),(4695,1933),(4448,1724),(4876,1457),(4562,1400),
+    (4762,924),(4486,1086),(4486,600),(4267,819),(3990,400),
+    (4000,733),
 ]
-
 
 # Define the world dimensions with required angle
-coordinates = [
-    (2781,533,1),(848,571,30),(581,705,50),(400,886,70),(295,1105,82),
-    (248,2086,91),(267,3486,109),(343,3762,124),(505,4010,141),(743,4229,154),
-    (1000,4371,170),(2324,4438,-180),(4190,4381,-151),(4543,4143,-120),(4714,3819,-91),
-    (4733,3495,-67),(4610,3190,-45),(4400,2924,-33),(4171,2714,-27),(3952,2533,-34),
-    (3714,2305,-35),(3495,2076,-32),(3286,1876,-32),(3067,1648,-36),(2733,1419,-10),
-    (2305,1381,21),(1895,1581,54),(1676,1905,81),(1619,2390,92),(1638,2952,116),
-    (1752,3210,132),(1981,3438,153),(2343,3600,178),(2762,3581,-153),(3124,3381,-126),
-    (3410,3086,-129),(3781,2705,-129),(4143,2324,-127),(4419,2038,-126),(4657,1686,-104),
-    (4714,1257,-70),(4552,867,-42),(4248,629,-14),
+centroids = [
+    (3829,533,7),(3410,533,8),(3048,533,9),(2686,533,8),(2314,533,10),
+    (1924,533,8),(1505,533,9),(1143,533,11),(848,571,30),(581,705,50),
+    (400,886,70),(295,1105,82),(248,1381,97),(248,1762,97),(248,2133,97),
+    (248,2505,98),(248,2857,97),(248,3200,97),(267,3486,109),(343,3762,124),
+    (505,4010,141),(743,4229,154),(1000,4371,170),(1362,4429,-175),(1781,4438,-173),
+    (2171,4448,-174),(2552,4457,-172),(2933,4457,-172),(3362,4448,-174),(3781,4448,-174),
+    (4190,4381,-151),(4543,4143,-120),(4714,3819,-91),(4733,3495,-67),(4610,3190,-45),
+    (4400,2924,-33),(4171,2714,-27),(3952,2533,-34),(3714,2305,-35),(3495,2076,-32),
+    (3286,1876,-32),(3067,1648,-36),(2733,1419,-10),(2305,1381,21),(1895,1581,54),
+    (1676,1905,81),(1619,2238,95),(1610,2629,97),(1638,2952,116),(1752,3210,132),
+    (1981,3438,153),(2343,3600,178),(2762,3581,-153),(3124,3381,-126),(3410,3086,-129),
+    (3781,2705,-129),(4143,2324,-127),(4419,2038,-126),(4657,1686,-104),(4714,1257,-70),
+    (4552,867,-42),(4248,629,-14),
 ]
-
 
 # ENVIRONMENT --------------------------------------------------------------------------------------------------------
 
@@ -123,7 +132,7 @@ class EyeSimEnv(gym.Env):
         if result2 > 0:
             # update previous polygon to current polygon and repeat 
             current_centroid += 1
-            current_centroid %= len(coordinates)
+            current_centroid %= len(centroids)
             update_polygon()
             return 1.0
         if result1 < 0 and result2 < 0: 
@@ -165,19 +174,19 @@ def eyesim_get_observation():
 def update_polygon():
     global current_polygon, next_polygon, next_centroid
     current_polygon = np.array([
-        flipped_left_lane_world_coordinates[current_centroid*2],
-        flipped_left_lane_world_coordinates[current_centroid*2+1],
-        flipped_left_lane_world_coordinates[(current_centroid*2+3)],
-        flipped_left_lane_world_coordinates[(current_centroid*2+2)]
+        left_lane[current_centroid*2],
+        left_lane[current_centroid*2+1],
+        left_lane[(current_centroid*2+3)],
+        left_lane[(current_centroid*2+2)]
     ], np.int32)
 
-    next_centroid = (current_centroid + 1)% len(coordinates)
+    next_centroid = (current_centroid + 1)% len(centroids)
 
     next_polygon = np.array([
-        flipped_left_lane_world_coordinates[next_centroid*2],
-        flipped_left_lane_world_coordinates[next_centroid*2+1],
-        flipped_left_lane_world_coordinates[(next_centroid*2+3)],
-        flipped_left_lane_world_coordinates[(next_centroid*2+2)]
+        left_lane[next_centroid*2],
+        left_lane[next_centroid*2+1],
+        left_lane[(next_centroid*2+3)],
+        left_lane[(next_centroid*2+2)]
     ], np.int32)
 
 
@@ -210,10 +219,10 @@ def eyesim_reset():
     # Stop robot movement
     VWSetSpeed(0,0)
 
-    current_centroid = current_centroid%len(coordinates)-1
+    current_centroid = current_centroid%len(centroids)-1
 
     # # Position the robot in the simulation
-    x,y,phi = coordinates[current_centroid]
+    x,y,phi = centroids[current_centroid]
 
     SIMSetRobot(1,x,y,10,phi+180) # Add 180 degrees to the angle to flip robot into correct direction
 
@@ -267,7 +276,7 @@ def train():
 
     # Train the model for 100,000 steps
     model.learn(total_timesteps=100000, progress_bar = True, reset_num_timesteps=False, tb_log_name=f"{algorithm}")
-    model.save(f"{models_dir}")
+    model.save(f"{models_dir}/model_0")
 
 # LOAD ---------------------------------------------------------------------------------------------------------------- 
 
@@ -275,7 +284,7 @@ def train():
 def load(): 
 
     # Load the pre-trained model
-    trained_model = 6
+    trained_model = "model_0"
     model_path = f"{models_dir}/{trained_model}.zip"
     model = PPO.load(model_path,env=env)
 
@@ -283,33 +292,43 @@ def load():
     done = False
     obs, _ = env.reset()
     while True:
+        
         if done:
             obs, _ = env.reset()
             done = False
-        LCDMenu("-", "-", "-", "STOP")
         action, _ = model.predict(obs)
         obs, reward, done, _, _ = env.step(action)
         print(f"Reward: {reward}, Done: {done}")
 
+        LCDMenu("-", "-", "-", "STOP")
         key = KEYRead()
         if key == KEY4: # Train the model
             break
+    
+
 # LOAD AND TRAIN --------------------------------------------------------------------------------------------------------
 
 # Function to load a pre-trained model and continue training it
 def load_train(): 
 
     # Load the pre-trained model
-    trained_model = 9
+    trained_model = "model_0"
     model_path = f"{models_dir}/{trained_model}.zip"
     model = PPO.load(model_path, env)
     
-    iterations = 10
     # Continue training the model
-    for i in range(trained_model + 1, trained_model + 1 + iterations):
-        LCDMenu("-", "-", "-", "STOP")
-        model.learn(total_timesteps=10000, progress_bar = True, reset_num_timesteps=False, tb_log_name=f"{algorithm}")
-        model.save(f"{models_dir}/{i}")        
+    i = 0
+    while True:
+        LCDMenu("TRAIN", "-", "-", "STOP")
+        key = KEYRead()
+        if key == KEY1: # Train the model
+            model.learn(total_timesteps=50000, progress_bar = True, reset_num_timesteps=False, tb_log_name=f"{algorithm}")
+            new_model = f"model_{i}"
+            model.save(f"{models_dir}/{new_model}")
+            i += 1
+        elif key == KEY4:
+            break
+
         
 # IMAGE PROCESSING -------------------------------------------------------------------------------------------------------
 
@@ -338,19 +357,28 @@ def main():
     LCDImageStart(0,0,CAMWIDTH,DESIRED_CAMHEIGHT)
 
     while True:
-        LCDMenu("TRAIN", "LOAD", "TEST", "STOP")
+        LCDMenu("TRAIN", "TEST", "LOAD", "STOP")
 
         key = KEYRead()
-        if key == KEY1: # Train the model
+        if key == KEY1: # Train the modelG", "-", "-", "STOP")
             train()
 
         elif key == KEY2: # Load a pre-trained model and test it
-            load()
+            test()
 
         elif key == KEY3: # Test the environment and the robot's performance
-            test()
+            while True:
+                LCDMenu("LOAD_TEST", "LOAD_TRAIN", "-", "STOP")
+                key = KEYRead()
+                if key == KEY1: # Load a pre-trained model and test it
+                    load()
+                elif key == KEY2: # Load a pre-trained model and continue training it
+                    load_train()
+                elif key == KEY4: # Stop the program
+                    break
             
         elif key == KEY4: # Stop the program
             break
 
 main()
+
